08d9476955a022fa86f7b7da0cad02b13ecd0ccb
AlexanderHopkins67 <AlexanderHopkins67@gmail.com>
2023-01-09 17:41:35 -0800

    started character sheet

---
+++ jack-of-all-trades/src/Components/CharacterSheet.js
@@ -0,0 +1,19 @@
+import react from "react"
+
+export default function CharacterSheet(props) {
+    <div className="cs-body">
+        
+        <div className="cs-stats">
+            
+        </div>
+        <div className="cs-attributes">
+
+        </div>
+        <div className="cs-actions">
+        
+        </div>
+        <div className="cs-level">
+        
+        </div>
+    </div>
+}
